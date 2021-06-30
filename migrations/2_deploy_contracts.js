9897f60117d3ce3b7e022422c0e288d8f4228aa9
Muritala David <davmuri1414@gmail.com>
2021-06-30 01:48:46 -0700

    transfering token

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,15 @@
 const EthSwap = artifacts.require("EthSwap");
 const Token = artifacts.require("Token");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer) {
   //Deploy the token to a blockchain
   deployer.deploy(Token);
+  const token = await Token.deployed();
 
   //Deplloy the contract that handles the exchange (token exchange service)
   deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  //Transfer all tokens to the major ethswap wallet
+  await token.transfer(ethSwap.address, '100000000000000000');
 };
