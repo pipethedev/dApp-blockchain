a347ea6a16bd92ba9b4f1aab2428156c6fab9665
Muritala David <davmuri1414@gmail.com>
2021-06-30 01:43:08 -0700

    added token to migrations/deploy_contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
 
 module.exports = function(deployer) {
+  //Deploy the token to a blockchain
+  deployer.deploy(Token);
+
+  //Deplloy the contract that handles the exchange (token exchange service)
   deployer.deploy(EthSwap);
 };
