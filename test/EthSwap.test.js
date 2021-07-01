6342adfc7be77c76f0edbc9e28c103f42cd45cb8
Muritala David <davmuri1414@gmail.com>
2021-07-01 11:54:36 -0700

    ran few tests

--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -7,9 +7,25 @@ contract('EthSwap', (accounts) => {
 
     describe('EthSwap deployment', async() => {
         it('contract has a name', async() => {
+            let token = await Token.new();
+            const name = await token.name();
+            assert.equal(name, 'DApp Token')
+        })
+    })
+
+    describe('Token deployment', async() => {
+        it('contract has a name', async() => {
             let ethSwap = await EthSwap.new();
             const name = await ethSwap.name();
             assert.equal(name, 'EthSwap Exchange')
+        })
+
+        it('contract has a tokens', async() => {
+            let token = await Token.new();
+            const ethSwap = await EthSwap.new();
+            await token.transfer(ethSwap.address, '1000000000000000000000000')
+            let balance = await token.balanceOf(ethSwap.address)
+            assert.equal(balance.toString(), '1000000000000000000000000')
         })
     })
 });
