b51044cf3cbdecd3a0b6ae31dfefaa4ab74fd12c
Muritala David <davmuri1414@gmail.com>
2021-07-01 18:18:38 -0700

    sell tokens

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -13,9 +13,33 @@ contract EthSwap {
         token = _token;
     }
 
+    TokenPurchased(
+        address account,
+        address token,
+        uint amount,
+        uint rate
+    )
+
     function buyTokens() public payable{
         //Calculate tokens
         uint tokenAmount = msg.value * rate;
+
+        //Ensure master wallet have enough tokens
+        require(token.balanceOf(address(this)) >= tokenAmount);
+
+        //Transfer all tokens to user
         token.transfer(msg.sender, tokenAmount);
+
+        //Emit an event
+        TokenPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens() public {
+        //Calculate the amount of tokens to get back
+        uint etherAmount = _amount / rate ;
+
+        //Perform the sale
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount)
     }
 }
