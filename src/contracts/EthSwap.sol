4ff62f0fe01250cd7a2c6542f872c9a8af2a4ede
Muritala David <davmuri1414@gmail.com>
2021-07-01 17:22:29 -0700

    tests partically complete

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.0;
 
+import "./Token.sol";
+
 contract EthSwap {
     string public name = "EthSwap Exchange";
+    Token public token;
+
+    constructor(Token _token) public {
+        token = _token;
+    }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -1,31 +1,41 @@
+const { default: Web3 } = require('web3');
+
 const EthSwap = artifacts.require("EthSwap");
 const Token = artifacts.require("Token");
 
 require('chai').use(require('chai-as-promised')).should();
 
-contract('EthSwap', (accounts) => {
+function tokens(n){
+    return web3.utils.toWei(n, 'ether');
+}
 
-    describe('EthSwap deployment', async() => {
+contract('EthSwap', (accounts) => {
+    let token, ethSwap;
+
+    before(async () => {
+        token = await Token.new()
+        ethSwap = await EthSwap.new()
+
+        //Transfer 1M
+        await token.transfer(ethSwap.address, tokens('1000000'))
+    })
+
+    describe('Token deployment', async() => {
         it('contract has a name', async() => {
-            let token = await Token.new();
             const name = await token.name();
             assert.equal(name, 'DApp Token')
         })
     })
 
-    describe('Token deployment', async() => {
+    describe('EthSwap deployment', async() => {
         it('contract has a name', async() => {
-            let ethSwap = await EthSwap.new();
             const name = await ethSwap.name();
             assert.equal(name, 'EthSwap Exchange')
         })
 
         it('contract has a tokens', async() => {
-            let token = await Token.new();
-            const ethSwap = await EthSwap.new();
-            await token.transfer(ethSwap.address, '1000000000000000000000000')
             let balance = await token.balanceOf(ethSwap.address)
-            assert.equal(balance.toString(), '1000000000000000000000000')
+            assert.equal(balance.toString(), tokens('1000000'))
         })
     })
 });
