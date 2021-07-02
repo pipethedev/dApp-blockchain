785558abb7bafe51598ce35c1b7f094cf4c82299
Muritala David <davmuri1414@gmail.com>
2021-07-01 17:26:24 -0700

    started buy tokens

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -6,7 +6,16 @@ contract EthSwap {
     string public name = "EthSwap Exchange";
     Token public token;
 
+    //Fixed exchange rate
+    uint public rate = 100;
+
     constructor(Token _token) public {
         token = _token;
+    }
+
+    function buyTokens() public payable{
+        //Calculate tokens
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
     }
 }
